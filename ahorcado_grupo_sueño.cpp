2ae0445694a496270621ea4b966269d1bb3df654
SebastianMT1 <00161024@uca.edu.sv>
2024-06-24 18:26:30 -0600

    funcion de numero aleatorio

--- ahorcado_grupo_sueño.cpp
+++ ahorcado_grupo_sueño.cpp
@@ -5,7 +5,24 @@
 
 using namespace std;
 
+//Asignar un numero aleatorio entre 1 y 5 para las palabras de cada dificultad
+int numero_aleatorio();
+
+//Funcion para elegir la palabra dependiendo de la dificultad elegida por el usuario
+string seleccion_palabra(int x);
+
 int main(){
 
     return 0;
 }
+
+int numero_aleatorio(){
+    //Usando la hora del computador del usuario se saca el numero aleatorio
+    int valor = time(0);
+    srand(valor);
+    //Ese numero se divide entre 5 (los arreglos comienzan desde 0) porque son 5 palabras por dificultad
+    int asignar_palabra = rand() % 5;
+
+    return asignar_palabra;
+}
+
