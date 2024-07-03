07a184b791677b75c6894ed949bf049b4624b381
FernandoEscam <00053324@uca.edu.sv>
2024-07-03 10:55:34 -0600

    nueva funcion de jugadores

--- menuaddjugadores.cpp
+++ menuaddjugadores.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 using namespace std;
-const int MAX_JUGADORES=4;
-struct jugador{
-      string nombre;
-      int numFallos;
+//indicamos la estructura a utilizar en la funcion, indicando primeramente el maximo de jugadores
+const int MAX_JUGADORES = 4;
+
+struct Jugador {
+    string nombre;
+    int numFallos;
 };
-jugador jugadores[MAX_JUGADORES];
-int addjugadores(){
+
+Jugador jugadores[MAX_JUGADORES];
+
+int agregarJugadores() {
     int n;
-    do
-    {
-        cout<<"cuantas personas van a jugar? max 4";
-        cin>> n;
-        if(n<1|| n> MAX_JUGADORES){
-            cout<<"numero de jugadores no valido. intente otra vez""\n";
+    //un do while, cuando se le pide al usuario el numero de jugadores, si ingresa otro numero 
+    //que no este en el rango de (1-4) se repite.
+    do {
+        cout << "Cuantos jugadores van a jugar? (1-4): ";
+        cin >> n;
+
+        if (n < 1 || n > MAX_JUGADORES) {
+            cout << "Numero de jugadores no valido. Intente nuevamente." << endl;
         }
-    } while (n<1||n>MAX_JUGADORES);
-
-    for(int i=0; i<n; i++ ){
-        cout<<"agregue el nombre del jugador "<<(i+1)<<": ";
-        cin>>jugadores[i].nombre;
-        jugadores[i].numFallos=0;
+    } while (n < 1 || n > MAX_JUGADORES);
+    //un bucle for para gregar los nombres de los jugadores segun el numero introducido.
+    for (int i = 0; i < n; i++) {
+        cout << "Agregue el nombre del jugador " << (i + 1) << ": ";
+        cin >> jugadores[i].nombre;
+        jugadores[i].numFallos = 0; // Inicializamos numFallos a 0
     }
-    cout<<"jugadores agregados:"<<"\n";
-    for(int i=0;i<n;i++){
-        cout<<"Jugador "<<i+1<<": "<<jugadores[i].nombre<<"\n";
 
+    cout << "Jugadores agregados:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Jugador " << (i + 1) << ": " << jugadores[i].nombre << endl;
     }
+
     return n;
 }
